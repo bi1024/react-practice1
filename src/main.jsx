2754a3e6ea0fa7b84b5f4328f9bfadbab58555da
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2024-10-02 08:16:38 +0700

    install packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 // import './index.css'
 import ErrorBoundary from "./components/ErrorBoundary";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <ErrorBoundary fallback={<p>Something went wrong</p>}>
+    <ErrorBoundary fallback={<p>Something went wrong</p>}>
+      <QueryClientProvider client={queryClient}>
         <App />
-      </ErrorBoundary>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ErrorBoundary>
   </StrictMode>
 );
