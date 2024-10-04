ee6680cdbec76bd2292e3d2390712aa0518bc18f
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2024-10-04 15:58:27 +0700

    changing categories dropdown to fetch specific url as opposed to sorting through the whole list

--- src/services/services.js
+++ src/services/services.js
@@ -1,3 +1,5 @@
+// import { getCategories } from "../utils/productList";
+
 const apiUrl = import.meta.env.VITE_API_URL;
 
 
@@ -17,6 +19,82 @@ export const fetchProducts = async () => {
   }
 }
 
+export const fetchCategories = async () => {
+  try {
+    const response = await fetch(`${apiUrl}/categories`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    let result = await response.json()
+    return result;
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    return [];//ensure no reading of undefined => page isn't borked
+  }
+}
+
+export const fetchProductsInCategory = async (category) => {
+  try {
+    if (category && category.length > 0) {
+      const response = await fetch(`${apiUrl}/products?category=${category}`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      let result = await response.json()
+      return result;
+    } else { return fetchProducts() }
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    return [];//ensure no reading of undefined => page isn't borked
+  }
+}
+
+// export const fetchProductsInCategoryWithPagination = async (category, page, limit) => {
+//   try {
+//     if (category && category.length > 0) {
+//       const response = await fetch(`${apiUrl}/products?category=${category}&_page=${page}&_limit=${limit}`);
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+//       let result = await response.json()
+//       return {data:result};
+//     } else {
+//       const response = await fetch(`${apiUrl}/products?_page=${page}&_limit=${limit}`);
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+//       let result = await response.json()
+//       return result;
+//     }
+//   } catch (error) {
+//     console.log(`Error: ${error}`);
+//     return [];//ensure no reading of undefined => page isn't borked
+//   }
+// }
+export const fetchProductsInCategoryWithPagination = async (category, page, limit) => {
+  try {
+    if (category && category.length > 0) {
+      const response = await fetch(`${apiUrl}/products?category=${category}&_page=${page}&_limit=${limit}`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      let headers = response.headers.get("x-total-count");
+      let result = response.json()
+      return { data: result, headers: headers };
+    } else {
+      const response = await fetch(`${apiUrl}/products?_page=${page}&_limit=${limit}`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      let headers = response.headers.get("x-total-count");
+      let result = response.json()
+      return { data: result, headers: headers };
+    }
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    return [];//ensure no reading of undefined => page isn't borked
+  }
+}
 
 export const fetchProductById = async (id) => {
   try {
@@ -74,3 +152,4 @@ export const addProduct = async (product) => {
     console.log(`Error: ${error}`);
   }
 }
+
